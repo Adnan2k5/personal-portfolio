d4490f909a565be12db3b16fd1914c0200b7365d
Adnan Ashraf <ashrafadnan959@gmail.com>
2024-08-28 01:54:11 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,53 +1,3 @@
-// import React from "react";
-// import { Layout } from "../Layout/Layout";
-// import Typewriter from "typewriter-effect";
-// import vector from "../assets/vector.png";
-// import { Skills } from "./Skills";
-// import { Projects } from "./projects";
-
-// export const Home = () => {
-//   return (
-//     <div>
-//       <Layout>
-//         <div id="home" className="home-box w-[100vw] h-[80vh] mt-[10vh] z-10 flex absolute">
-//           <div className="text-side flex items-center text-center text-4xl p-5 gap-4 w-[55%]">
-//             <div className="text -mt-[11rem] w-full gap-3 ">
-//               <h1>
-//                 Adnan Ashraf, <br />{" "}
-//               </h1>
-//               <div className="typewriter text-cyan-300">
-//                 <Typewriter
-//                   options={{
-//                     strings: ["MERN Stack", "AI/ML", "4 * Hacker Rank"],
-//                     autoStart: true,
-//                     loop: true,
-//                   }}
-//                 />
-//               </div>
-//               <h1 className="text-xl mt-5">
-//                 B. Tech Cse <br /> KL University
-//               </h1>
-//             </div>
-//           </div>
-//           <div className="vector-side  relative -ml-[12rem] -z-10  flex justify-start  h-[80vh]">
-//             <img
-//               className="w-full h-full rounded-full object-cover"
-//               src={vector}
-//               alt="vector"
-//             ></img>
-//           </div>
-//         </div>
-//         <div className="skills mt-[90vh]">
-//           <Skills/>
-//         </div>
-//         <div className="projects">
-//           <Projects/>
-//         </div>
-//       </Layout>
-//     </div>
-//   );
-// };
-
 import React from "react";
 import { Layout } from "../Layout/Layout";
 import Typewriter from "typewriter-effect";
@@ -95,7 +45,7 @@ export const Home = () => {
           <Skills />
         </div>
 
-        <div className="projects">
+        <div className="projects mt-[30vh]">
           <Projects />
         </div>
       </Layout>
